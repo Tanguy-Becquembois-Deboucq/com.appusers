54045231bd21a64888c68b17bf5f0ed4d2fe4346
Tanguy Becquembois Deboucq <tanguydeboucq@gmail.com>
2022-06-07 16:34:56 +0200

    dark/light mode beta
    cela marche moyen, il faut changer la taille de l'icon et il ne se souvient pas du changement

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ require('core/conn.php.inc');
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+      <link rel="stylesheet" href="dark-mode3.css">
   </head>
   <body>
     <?php 
@@ -18,5 +19,18 @@ if ($page == 'profil') { require('views/profil.php.inc'); }
     ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
   </script>
+  <script>
+    var icon = document.getElementById('icon');
+
+    icon.onclick = function(){
+      document.body.classList.toggle("dark-theme");
+      if(document.body.classList.contains("dark-theme")){
+        icon.src ="darkthemeicon/sun.png";
+      } else {
+        icon.src = "darkthemeicon/moon.png";
+      }
+    }
+  </script>
   </body>
+  <footer></footer>
 </html>
