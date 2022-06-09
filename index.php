69176467dbbbdb0533a5157669c4a3cb2c0b265d
Tanguy Becquembois Deboucq <tanguydeboucq@gmail.com>
2022-06-09 16:58:06 +0200

    Loading marche 50%
    cela marche sur le PC mais pas sur le tel

--- index.php
+++ index.php
@@ -11,7 +11,10 @@ require('core/conn.php.inc');
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
       <link rel="stylesheet" href="dark-mode.css">
   </head>
-  <body>
+  <body class="load">
+    <div class="bg-secondary text-white d-flex align-items-center justify-content-center" id="loading">
+      <p>Loading...</p>
+    </div>
     <?php 
 if ($page == '') { require('views/list.php.inc'); }
 if ($page == 'add') { require('views/form.php.inc'); }
@@ -65,6 +68,13 @@ function getCookie(cname) {
       }
 
     }
+
+    var loading = document.getElementById('loading');
+        setTimeout(function (){
+    loading.classList.add('bye');
+    document.body.classList.remove('load');
+    }, 2000);
+
   </script>
   </body>
   <footer></footer>
